2ddcca7259195e36933b5d55799d876167817e8e
Prakash842277 <121437397+Prakash842277@users.noreply.github.com>
2023-04-13 12:05:49 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,9 +1,19 @@
-const fs = require("fs/promises");
-const fileName = "myfile.txt";
+// const fs = require("fs/promises");
+// const fileName = "myfile.txt";
+// const updateFile = async (fileName, fileContent) => {
+//   // write code here
+//   // dont change function name
+  
+// };
+
+// module.exports = updateFile;
+const fs = require(“fs/promises”);
+const fileName = “myfile.txt”;
 const updateFile = async (fileName, fileContent) => {
   // write code here
   // dont change function name
-  
+  return await fs.appendFile(fileName, fileContent);
 };
+updateFile(fileName, “\n, An online learning platform”);
 
 module.exports = updateFile;
